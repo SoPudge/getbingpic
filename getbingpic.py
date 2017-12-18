b37af9ee882241c1560284df82e430c8e0cccd7c
SoPudge <abcd666@gmail.com>
2017-12-18 23:12:30 +0800

    [EDIT]完善抓取逻辑，需要验证状态码以跳过无图的链接

--- getbingpic.py
+++ getbingpic.py
@@ -9,21 +9,45 @@ class bing(object):
 
     def getpic(self,ds=0):
         urls = []
-        [urls.append('https://bing.ioliu.cn/v1/?d=%s&type=json' % d) for d in range(ds)]
-        re_pname = re.compile(r'(/rb/)\w{0,10}')
+        [urls.append('https://bing.ioliu.cn/v1/?d=%s&w=1920&h=1080&type=json' % d) for d in range(ds)]
+        re_pname = re.compile(r'(.*)rb/(.*?)(_.*)')
+        re_piourl = re.compile(r'(.*)&type=json')
             
-        r = requests.get(urls[4])
+        #for n in range(ds):
+        #    r = requests.get(urls[n])
+        #    pcontent = json.loads(r.content)
+        #    pdate = pcontent['data']['enddate']
+        #    pbingurl = pcontent['data']['url']
+        #    pname = re_pname.match(pbingurl).group(2) 
+        #    piourl = re_piourl.match(urls[n]).group(1)
+        #    #print(pdate)
+        #    #print(pname)
+        #    #print(pbingurl)
+        #    #print(urls[n])
+        #    #print(piourl)
+        #    pic = requests.get(piourl)
+        #    with open('%s.jpg' % pname,'wb') as file:
+        #        file.write(pic.content)
+        #    print('%s/%s|get the %s pic,name is %s' % (n,ds,pdate,pname))
+        #print('done')
+
+        n = 95
+        r = requests.get(urls[n])
         pcontent = json.loads(r.content)
         pdate = pcontent['data']['enddate']
-        pname = pcontent['data']['url']
-        print(type(pname))
-        print(re_pname.match(pname).groups())
-        #with open('%s.jpg' % d,'wb') as file:
-        #   file.write(r.content)
+        pbingurl = pcontent['data']['url']
+        pname = re_pname.match(pbingurl).group(2) 
+        piourl = re_piourl.match(urls[n]).group(1)
+        print(pdate)
+        print(pname)
+        print(pbingurl)
+        print(urls[n])
+        print(piourl)
+        pic = requests.get(piourl)
 
 if __name__ == '__main__':
     pic = bing()
-    pic.getpic(6)
+    pic.getpic(365)
 
 
 
