42ef1917e95f8ff988f5fe70738048203b973fab
SoPudge <abcd666@gmail.com>
2017-12-19 20:14:05 +0800

    [EDIT]完成壁纸抓取保存逻辑

--- getbingpic.py
+++ getbingpic.py
@@ -13,37 +13,29 @@ class bing(object):
         re_pname = re.compile(r'(.*)rb/(.*?)(_.*)')
         re_piourl = re.compile(r'(.*)&type=json')
             
-        #for n in range(ds):
-        #    r = requests.get(urls[n])
-        #    pcontent = json.loads(r.content)
-        #    pdate = pcontent['data']['enddate']
-        #    pbingurl = pcontent['data']['url']
-        #    pname = re_pname.match(pbingurl).group(2) 
-        #    piourl = re_piourl.match(urls[n]).group(1)
-        #    #print(pdate)
-        #    #print(pname)
-        #    #print(pbingurl)
-        #    #print(urls[n])
-        #    #print(piourl)
-        #    pic = requests.get(piourl)
-        #    with open('%s.jpg' % pname,'wb') as file:
-        #        file.write(pic.content)
-        #    print('%s/%s|get the %s pic,name is %s' % (n,ds,pdate,pname))
-        #print('done')
-
-        n = 95
-        r = requests.get(urls[n])
-        pcontent = json.loads(r.content)
-        pdate = pcontent['data']['enddate']
-        pbingurl = pcontent['data']['url']
-        pname = re_pname.match(pbingurl).group(2) 
-        piourl = re_piourl.match(urls[n]).group(1)
-        print(pdate)
-        print(pname)
-        print(pbingurl)
-        print(urls[n])
-        print(piourl)
-        pic = requests.get(piourl)
+        for n in range(ds):
+            r = requests.get(urls[n])
+            pcontent = json.loads(r.content)
+            piostatu = pcontent['status']['code']
+            if piostatu == -1:
+                print('%s/%s|no pic' % (n,ds))
+                continue
+            else:
+                pdate = pcontent['data']['enddate']
+                pbingurl = pcontent['data']['url']
+                pname = re_pname.match(pbingurl).group(2) 
+                piourl = re_piourl.match(urls[n]).group(1)
+                #print(pdate)
+                #print(pname)
+                #print(pbingurl)
+                #print(urls[n])
+                #print(piourl)
+                pic = requests.get(piourl)
+                with open('%s.jpg' % pname,'wb') as file:
+                    file.write(pic.content)
+                print('%s/%s|get the %s pic,name is %s' % (n,ds,pdate,pname))
+        print('done')
+             
 
 if __name__ == '__main__':
     pic = bing()
